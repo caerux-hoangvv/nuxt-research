f96362343e3ec33e450c2b1fe3d98340a4be7d5e
hoangvv <hoangvv@caerux.com>
2024-12-12 14:12:16 +0700

    feat: ignore ssg /veggie

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ["./inner_project"],
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+    extends: ["./inner_project"],
+    compatibilityDate: "2024-11-01",
+    devtools: { enabled: true },
 
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // additionalData: `
-          //   @import "@/assets/styles/_variables.scss";
-          //   @import "@/assets/styles/_mixins.scss";
-          // `
-        }
-      }
-    }
-  },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    // additionalData: `
+                    //   @import "@/assets/styles/_variables.scss";
+                    //   @import "@/assets/styles/_mixins.scss";
+                    // `
+                },
+            },
+        },
+    },
 
-  routeRules: {
-    '/': {ssr: true}, // SSR
-    '/fetch': {ssr: true, prerender: true}, // Pure CSR, no prerendering
-    '/fruit/**': {ssr: true},
-    '/fruit': {ssr: false}, // SSG
-  },
-  generate: {
-    exclude: ['/veggie', '/Veggie'],
-  },
+    routeRules: {
+        "/": { ssr: true }, // SSR
+        "/fetch": { ssr: true, prerender: true }, // Pure CSR, no prerendering
+        "/fruit/**": { ssr: true },
+        "/fruit": { ssr: false }, // SSG
+    },
+    nitro: {
+        prerender: {
+            ignore: ["/veggie", "/Veggie"],
+        },
+    },
 
-  modules: ['@nuxt/content']
-})
+    modules: ["@nuxt/content"],
+});
